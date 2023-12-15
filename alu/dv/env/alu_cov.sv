2e21fd7d6493faffb0d7f0a45cbfcf07f0ff8dae
Luis Javier Flores Gomez <myluis9705@gmail.com>
2023-12-15 14:09:57 -0800

    Adding ALU functional coverage

--- alu/dv/env/alu_cov.sv
+++ alu/dv/env/alu_cov.sv
@@ -6,18 +6,44 @@
     // Factory Registration
     `uvm_component_utils(alu_cov)
 
+    covergroup cg() with function sample (alu_seq_item txn);
+      cp_a: coverpoint txn.a {
+        bins zero = {'0};
+        bins max_value = {`MAX_VALUE};
+        bins min_value = {`MIN_VALUE};
+        bins positive_values[100] = {[1:`MAX_VALUE]};
+        bins negative_values[100] = {[`MIN_VALUE:-1]};
+      }
+      cp_b: coverpoint txn.b{
+        bins zero = {'0};
+        bins max_value = {`MAX_VALUE};
+        bins min_value = {`MIN_VALUE};
+        bins positive_values[100] = {[1:`MAX_VALUE]};
+        bins negative_values[100] = {[`MIN_VALUE:-1]};
+      }
+      cp_alu_op: coverpoint txn.alu_op;
+      cp_result: coverpoint txn.result;
+      cp_zero: coverpoint txn.zero;
+
+    endgroup
+
+
     extern function new(string name = "alu_cov", uvm_component parent = null);
     extern virtual function void write(alu_seq_item t);
   endclass
 
   function alu_cov::new(string name = "alu_cov", uvm_component parent = null);
     super.new(name, parent);
+    cg = new;
   endfunction
 
   function void alu_cov::write(alu_seq_item t);
     `uvm_info(get_full_name(), "[ALU] Received item in Subscriber", UVM_LOW)
     `uvm_info(get_full_name(), $sformatf("\n[ALU] Packet Data:\n\ta: %0d,\n\tb: %0d,\n\talu_op: %s,\n\tresult: %0d,\n\tzero: %0d",
       t.a, t.b, t.alu_op.name, t.result, t.zero), UVM_LOW)
+
+    cg.sample(t);
+
   endfunction
 
 `endif
